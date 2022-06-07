c188daf8eadb49e9c7ffa31f4d9af89131139543
Victor Gonzalez <victor.vgonzalez@mercadolibre.com>
2022-06-07 11:01:28 -0300

    Empty activity created

---
+++ app/src/main/java/com/example/meli_android_integrator/SuggestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.meli_android_integrator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuggestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suggestion)
+    }
+}
