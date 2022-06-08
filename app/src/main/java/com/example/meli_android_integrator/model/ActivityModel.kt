71e54111c507882fe60fd162a357ba03b9fb0eb5
Victor Gonzalez <victor.vgonzalez@mercadolibre.com>
2022-06-08 16:18:04 -0300

    The query to the api is implemented. Some graphic improvements to activity_suggestion.

--- app/src/main/java/com/example/meli_android_integrator/model/ActivityModel.kt
+++ app/src/main/java/com/example/meli_android_integrator/model/ActivityModel.kt
@@ -4,11 +4,11 @@ package com.example.meli_android_integrator.model
  * Modelo de datos que representa la respuesta del consumo
  */
 data class ActivityModel(
-    val accessibility: Double,
-    val activity: String,
-    val key: String,
-    val link: String,
-    val participants: Int,
-    val price: Double,
-    val type: String
+    val accessibility: Double = 0.0,
+    val activity: String = "",
+    val key: String = "",
+    val link: String = "",
+    val participants: Int = 0,
+    val price: Double = 0.0,
+    val type: String = ""
 )
